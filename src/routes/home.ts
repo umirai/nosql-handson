ebc89dc121527f4b33a288490bf62b0537c6ac64
UmedaMirai <umdmira128@gmail.com>
2022-08-21 13:40:55 +0900

    add: firestoreからデータを取得できることを確認

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,14 +1,18 @@
 import { FastifyInstance, RouteOptions } from 'fastify'
 import { applicationDefault, initializeApp } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
 
 async function routes(fastify: FastifyInstance, options: RouteOptions) {
-  fastify.get('/', function (request, reply) {
+  fastify.get('/', async function (request, reply) {
     const app = initializeApp({
       credential: applicationDefault(),
       databaseURL: `https://${fastify.config.DB_NAME}.firebaseio.com`
     })
 
-    reply.send('Hello World!' + app)
+    const db = getFirestore(app)
+    const querySnapshot = await db.collection('users').get()
+    const users = querySnapshot.docs.map(doc => doc.data())
+    reply.send(users)
   })
 }
 
